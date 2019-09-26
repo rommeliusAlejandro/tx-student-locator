96efe57151aba666bf86c266e05e57bfb0ffe471
rommel <rommel.lexlara@gmail.com>
2019-09-26 19:25:46 -0400

    fixing merge

--- src/main/java/com/rloayza/classroom/studentlocaltor/model/Input.java
+++ src/main/java/com/rloayza/classroom/studentlocaltor/model/Input.java
@@ -1,4 +1,26 @@
 package com.rloayza.classroom.studentlocaltor.model;
 
+import java.util.List;
+
 public class Input {
+
+    private List<Student> students;
+
+    private List<Classroom> classrooms;
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public List<Classroom> getClassrooms() {
+        return classrooms;
+    }
+
+    public void setClassrooms(List<Classroom> classrooms) {
+        this.classrooms = classrooms;
+    }
 }
--- src/main/java/com/rloayza/classroom/studentlocaltor/visitor/ClassroomVisitor.java
+++ src/main/java/com/rloayza/classroom/studentlocaltor/visitor/ClassroomVisitor.java
@@ -1,4 +1,26 @@
 package com.rloayza.classroom.studentlocaltor.visitor;
 
-public class ClassroomVisitor {
+import com.rloayza.classroom.studentlocaltor.model.Classroom;
+import com.rloayza.classroom.studentlocaltor.model.Student;
+
+import java.util.List;
+
+public class ClassroomVisitor implements Visitor {
+
+    private List<Student> students;
+
+    public ClassroomVisitor(List<Student> students) {
+        this.students = students;
+    }
+
+    public void visit(Visitable visitable) {
+        Classroom classroom = (Classroom) visitable;
+
+        StudentVisitor studentVisitor = new StudentVisitor(classroom.getLatitude(), classroom.getLongitude());
+
+        for (Student student: students) {
+            studentVisitor.visit(student);
+        }
+
+    }
 }
